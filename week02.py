7a18c0d6fb60a8b3f3dc743758d4393345e34e52
MeDeoDuck <seankim0824@inha.edu>
2026-03-10 18:51:12 +0900

    week02 average i

---
+++ week02.py
@@ -0,0 +1,4 @@
+# 파이썬 자체 함수 사용
+data = [10, 20, 30, 40]
+average = sum(data) / len(data)
+print(average)
---
+++ week03.py
@@ -0,0 +1,5 @@
+# 라이브러리 활용
+import statistics
+data = [10, 20, 30, 40]
+average = statistics.average(data)
+print(average)
