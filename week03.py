beed05b7068d37b17c13c3884f352a14b3e956cc
MeDeoDuck <seankim0824@inha.edu>
2026-03-17 18:48:35 +0900

    week03 dataframe II

--- week03.py
+++ week03.py
@@ -1,14 +1,35 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
 
-items = {
-    'a': [100, 80, 90],
-    'b': [95, 75, 85],
-    'c': [70, 100, 99]
-}
+mpg = sns.load_dataset('mpg')
+# print(mpg.head(3))
+# print(mpg.tail(3))
+# print(mpg.query('mpg > 17'))
+mpg_asc = mpg.sort_values('mpg')
+print(mpg_asc)
 
-df_items = pd.DataFrame(items, index=[1, 2, 3])
-print(df_items)
+# 'displacement' 날리기
+mpg_asx_disp_out = mpg_asc.drop(columns=['displacement'])
+print(mpg_asx_disp_out)
+
+# items = {
+#     'a': [100, 80, 90],
+#     'b': [95, 75, 85],
+#     'c': [70, 100, 99]
+# }
+
+# items = [
+#     [100, 95, 70],
+#     [80, 75, 100],
+#     [90, 85, 99]
+# ]
+# df_items = pd.DataFrame(items, index=[1, 2, 3], columns=['a', 'b', 'c'])
+# print(df_items)
+# df_items_melt = pd.melt(df_items).rename(columns={'variable': 'var', 'value': 'val'}).query('val >= 85')
+# print(df_items_melt)
+
+## 값의 변화가 없는 함수가 안전한 것 => 함수형 언어 사용
 
 # scores = np.array([
 #     # 국, 영, 수
